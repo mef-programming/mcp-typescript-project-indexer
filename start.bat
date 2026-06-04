5c08990750c3bb54cb5117fae04a65c5bcd0c1c3
Mike Walter <mw@smartftp.com>
2026-06-04 20:14:39 +0300

    removed local start.bat

--- start.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-node dist\http_server.js --project-root "c:\KI\mcp-model-relay" --index-root "c:\KI\mcp-model-relay\.mcp-ts-project-indexer" --http-host 127.0.0.1 --http-port 8766 --management-token test
